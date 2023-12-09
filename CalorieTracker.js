12fb8b390e296ce1cd94a4f700af5c2029e8a35a
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2023-12-09 16:00:52 -0500

    Update CalorieTracker.js

--- CalorieTracker.js
+++ CalorieTracker.js
@@ -1,15 +1,19 @@
 const apiKey = 'nlFarY62IO8Tzsv9VKYnkg==UJyqd9bpyem8X1Sq';
 
-function addFood() {
+document.getElementById('food').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById('add').click();
+    }
+});
+
+function addFood(event) {
+    event.preventDefault();
     const query = document.getElementById("food").value;
     const capitalizedQuery = capitalizeFirstLetter(query);
     console.log(capitalizedQuery);
 
-    const br = document.createElement("br");
-    document.getElementById("listOfFood").appendChild(br)
-
-    appendToList("listOfFood", capitalizedQuery);
-    fetchNutritionInfo(capitalizedQuery);
+    fetchNutritionInfo(capitalizedQuery, capitalizedQuery);
 }
 
 function capitalizeFirstLetter(string) {
@@ -22,13 +26,16 @@ function appendToList(listId, text) {
     document.getElementById(listId).appendChild(li);
 }
 
-function fetchNutritionInfo(query) {
+function fetchNutritionInfo(query, foodName) {
     $.ajax({
         method: 'GET',
         url: 'https://api.api-ninjas.com/v1/nutrition?query=' + query,
         headers: { 'X-Api-Key': apiKey },
         contentType: 'application/json',
-        success: handleApiResponse,
+        success: function(result) {
+            appendToList("listOfFood", foodName);
+            handleApiResponse(result);
+        },
         error: function(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
         }
@@ -44,23 +51,22 @@ function handleApiResponse(result) {
     appendToList("listOfFood", "Protein: " + protein + " grams");
 
     const totalFat = result[0].fat_total_g;
-    appendToList("listOfFood", "Total Fat: " + totalFat + " grams");
+    appendToList("listOfFood", "Total Fat: " + totalFat + " g");
 
     const totalCarbs = result[0].carbohydrates_total_g;
-    appendToList("listOfFood", "Total Carbs: " + totalCarbs + " grams");
+    appendToList("listOfFood", "Total Carbs: " + totalCarbs + " g");
 
     const servingSize = result[0].serving_size_g;
-    appendToList("listOfFood", "Serving Size: " + servingSize + " grams");
 
     const fiber = result[0].fiber_g;
-    appendToList("listOfFood", "Fiber: " + fiber + " grams");
+    appendToList("listOfFood", "Fiber: " + fiber + " g");
 
     const sugar = result[0].sugar_g;
-    appendToList("listOfFood", "Sugar: " + sugar + " grams");
+    appendToList("listOfFood", "Sugar: " + sugar + " g");
 
     const cholesterol = result[0].cholesterol_mg;
-    appendToList("listOfFood", "Cholesterol: " + cholesterol + " milligrams");
+    appendToList("listOfFood", "Cholesterol: " + cholesterol + " mg");
 
     const sodium = result[0].sodium_mg;
-    appendToList("listOfFood", "Sodium: " + sodium + " milligrams");
+    appendToList("listOfFood", "Sodium: " + sodium + " mg");
 }
