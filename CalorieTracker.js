cc654ab4ba425eef4f8f3fc96f69f8c84ea7b41f
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2023-12-13 12:22:11 -0500

    Update CalorieTracker.js

--- CalorieTracker.js
+++ CalorieTracker.js
@@ -26,9 +26,12 @@ function addFood(event) {
 
         document.getElementById('listOfFood' + currentList).classList.add('hasContent');
         currentList = (currentList % 10) + 1; 
+        document.getElementById('totalMacrosContainer').classList.add('hasContent');
     }
     document.getElementById('food').value = '';
 }
+
+
 
 function resetButton(event){ 
     event.preventDefault();
@@ -127,5 +130,48 @@ function handleApiResponse(result, listId, foodName) {
         "Total Sugar: " + totalSugar + " grams | " +
         "Total Cholesterol: " + totalCholesterol + " milligrams | " +
         "Total Sodium: " + totalSodium + " milligrams";
-}
 
+        var ctx = document.getElementById('percentChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Calories', 'Protein', 'Total Fat', 'Total Carbs', 'Fiber', 'Sugar', 'Cholesterol', 'Sodium'],
+                datasets:  [{ 
+                    data: [totalCalories, totalProtein, totalTotalFat, totalTotalCarbs, totalFiber, totalSugar, totalCholesterol, totalSodium],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        labels: {
+                            color: 'white',
+                            font:{
+                                size:20
+                            }
+                        }
+                    }
+                }
+            }
+        });
+}
